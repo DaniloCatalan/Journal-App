d4d498fe4192a3f296ccb080be953d0396818e99
Danilo P. Catalan <danilo.e.catalan@gmail.com>
2022-09-12 10:01:17 -0300

    enable note to edit

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,20 +1,30 @@
-import { SaveOutlined } from "@mui/icons-material";
-import { Button, Grid, TextField, Typography } from "@mui/material";
-import { ImageGallery } from "../components";
+import { SaveOutlined } from '@mui/icons-material';
+import { Button, Grid, TextField, Typography } from '@mui/material';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { useForm } from '../../hooks';
+import { ImageGallery } from '../components';
 
 export const NoteView = () => {
+  const { active: note } = useSelector((state) => state.journal);
+  const { body, title, onInputChange, formState, date } = useForm(note);
+
+  const dateString = useMemo(() => {
+    const newDate = new Date(date)
+    return newDate.toUTCString();
+  }, [date]);
+
   return (
     <Grid
       container
-      direction={"row"}
+      direction={'row'}
       justifyContent="space-between"
-      alignItems={"center"}
+      alignItems={'center'}
       sx={{ mb: 1 }}
       className="animate__animated animate__fadeIn animate__faster"
     >
       <Grid item>
-        <Typography fontSize={39} fontWeight={"light"}>
-          28 de Ago 2023
+        <Typography fontSize={39} fontWeight={'light'}>{dateString}
         </Typography>
       </Grid>
       <Grid item>
@@ -25,23 +35,29 @@ export const NoteView = () => {
       </Grid>
       <Grid container>
         <TextField
-          type={"text"}
+          type={'text'}
           variant="filled"
           fullWidth
           placeholder="Ingrese un titulo"
           label="titulo"
-          sx={{ border: "none", mb: 1 }}
+          sx={{ border: 'none', mb: 1 }}
+          name="title"
+          value={title}
+          onChange={onInputChange}
         />
         <TextField
-          type={"text"}
+          type={'text'}
           variant="filled"
           fullWidth
           multiline
           placeholder="Que sucede en el dia de hoy"
           minRows={5}
+          name="body"
+          value={body}
+          onChange={onInputChange}
         />
       </Grid>
-      <ImageGallery/>
+      <ImageGallery />
     </Grid>
   );
 };
